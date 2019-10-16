ca014604047feb97a46776994528f2369d861b62
tamartwe <tamartwena@gmail.com>
2019-10-16 21:25:49 +0300

    more tensorflow changes

--- image_classification_demo/index.js
+++ image_classification_demo/index.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs')
-require('@tensorflow/tfjs-node');
+const tf_node = require('@tensorflow/tfjs-node');
 const mobilenet = require('@tensorflow-models/mobilenet');
 
 const fs = require('fs');
@@ -7,33 +7,6 @@ const jpeg = require('jpeg-js');
 
 const NUMBER_OF_CHANNELS = 3
 
-const readImage = path => {
-  const buf = fs.readFileSync(path)
-  const pixels = jpeg.decode(buf, true)
-  return pixels
-}
-
-const imageByteArray = (image, numChannels) => {
-  const pixels = image.data
-  const numPixels = image.width * image.height;
-  const values = new Int32Array(numPixels * numChannels);
-
-  for (let i = 0; i < numPixels; i++) {
-    for (let channel = 0; channel < numChannels; ++channel) {
-      values[i * numChannels + channel] = pixels[i * 4 + channel];
-    }
-  }
-
-  return values
-}
-
-const imageToInput = (image, numChannels) => {
-  const values = imageByteArray(image, numChannels)
-  const outShape = [image.height, image.width, numChannels];
-  const input = tf.tensor3d(values, outShape, 'int32');
-
-  return input
-}
 
 const loadModel = async () => {
   const mn = await mobilenet.load();
@@ -41,10 +14,9 @@ const loadModel = async () => {
 }
 
 const classify = async (path) => {
-  const image = readImage(path)
-  const input = imageToInput(image, NUMBER_OF_CHANNELS)
-
-  const  mn_model = await loadModel()
+  const image_buf = fs.readFileSync(path);
+  const input = tf_node.node.decodeJpeg(image_buf);
+  const  mn_model = await loadModel();
   const predictions = await mn_model.classify(input)
 
   console.log('classification results:', predictions[0].className)
