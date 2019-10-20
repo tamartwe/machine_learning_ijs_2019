d527c54a3ed129381f0574cef243ab698db12698
tamartwe <tamartwena@gmail.com>
2019-10-20 19:33:35 +0200

    changes

--- image_classification_client/image-classification-client/src/app/image-list.ts
+++ image_classification_client/image-classification-client/src/app/image-list.ts
@@ -6,5 +6,7 @@ export const IMAGES: ImageContainer[] = [
     {imageTitle: 'Image 3', path :'assets/images/ice-cream.jpg',prediction: 'I have no idea'},
     {imageTitle: 'Image 4', path :'assets/images/daisy-vase.jpg',prediction: 'I have no idea'},
     {imageTitle: 'Image 5', path :'assets/images/balloons.jpg',prediction: 'I have no idea'},
-    {imageTitle: 'Image 6', path :'assets/images/mountains.jpg',prediction: 'I have no idea'}
+    {imageTitle: 'Image 6', path :'assets/images/butterfly.jpg',prediction: 'I have no idea'},
+    {imageTitle: 'Image 7', path :'assets/images/mountains.jpg',prediction: 'I have no idea'},
+    {imageTitle: 'Image 8', path :'assets/images/christmas-cookies.jpg',prediction: 'I have no idea'}
 ];
